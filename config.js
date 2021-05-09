e3e3f6e934e641268613ac58803c9eaf9479da1b
hilmanski <hilman@hilman.com>
2021-05-09 09:56:41 +0800

    remove api url from env

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let config = {}
 
-config.GITHUB_API_URL = process.env.GITHUB_API_URL
-config.GITHUB_OAUTH_URL = process.env.GITHUB_OAUTH_URL
+config.GITHUB_API_URL = "https://api.github.com"
+config.GITHUB_OAUTH_URL = "https://github.com/login/oauth"
 config.GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID
 config.GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET
 
